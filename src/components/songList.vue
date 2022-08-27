cd2ca1e5397c0cb1e7d4dc2f9ddf038e4f040306
Daniel Rodriguez Aires <dannroda@gmail.com>
2022-08-27 13:05:21 -0300

    Initial boilerplater

---
+++ src/components/songList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+var spotify_client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+var spotify_client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET
+
+console.log(spotify_client_id)
+
+
+    // <!-- <button type="button" @click="count++">count is {{ count }}</button> -->
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
